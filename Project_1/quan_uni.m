32ee5e9ec3843b67034170d77ada86015e9986c6
lehoangnam1803 <62412792+lehoangnam1803@users.noreply.github.com>
2024-01-11 15:44:13 +0700

    Fix

--- Project_1/quan_uni.m
+++ Project_1/quan_uni.m
@@ -1,17 +1,17 @@
-function quan_sig = quan_uni(sig,q)
-     for i = 1:length(sig)
-         quan_sig(i) = quant(sig(i),q);
-         d = sig(i) - quan_sig(i);
-         if d == 0
-             quan_sig(i) = quan_sig(i) + q/2;
-             elseif (d > 0) && (abs(d) < q/2)
-             quan_sig(i) = quan_sig(i) + q/2;
-             elseif (d > 0) && (abs(d) >= q/2)
-             quan_sig(i) = quan_sig(i) - q/2;
-             elseif (d < 0) && (abs(d) < q/2)
-             quan_sig(i) = quan_sig(i) - q/2;
-             elseif (d < 0) && (abs(d) >= q/2)
-             quan_sig(i) = quan_sig(i) + q/2;
-         end
-     end
+function quan_sig = quan_uni(sig, q)
+    for i = 1:length(sig)
+        quan_sig(i) = quant(sig(i), q);
+        d = sig(i) - quan_sig(i);
+        if d == 0
+            quan_sig(i) = quan_sig(i) + q/2;
+        elseif (d > 0) && (abs(d) < q/2)
+            quan_sig(i) = quan_sig(i) + q/2;
+        elseif (d > 0) && (abs(d) >= q/2)
+            quan_sig(i) = quan_sig(i) - q/2;
+        elseif (d < 0) && (abs(d) < q/2)
+            quan_sig(i) = quan_sig(i) - q/2;
+        elseif (d < 0) && (abs(d) >= q/2)
+            quan_sig(i) = quan_sig(i) + q/2;
+        end
+    end
 end
